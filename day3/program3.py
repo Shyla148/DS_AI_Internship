b642ad97c386a277db721d97704db2a01ef120b5
Shyla148 <shylashyla161@gmail.com>
2026-08-07 10:02:32 +0530

    c2

--- day3/program3.py
+++ day3/program3.py
@@ -1,9 +1,7 @@
-# Function to print table
+
 def table(n):
     print("\nMultiplication Table of", n)
     for i in range(1, 11):
         print(n, "x", i, "=", n * i)
-
-# Main Program
 num = int(input("Enter a number: "))
 table(num)
