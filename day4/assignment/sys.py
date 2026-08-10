fd53c2a3b06016da2073e4f961de9de872d6553e
Shyla148 <shylashyla161@gmail.com>
2026-08-10 10:47:39 +0530

    commit6

---
+++ day4/assignment/sys.py
@@ -0,0 +1,11 @@
+import sys
+
+def get_python_environment_info():
+    """Returns the current Python version and operating system platform."""
+    return {
+        "python_version": sys.version.split()[0],
+        "platform": sys.platform
+    }
+
+info = get_python_environment_info()
+print(f"Python Version: {info['python_version']} on {info['platform']}")
